cdc20f2c548c1aca5e418832eb07c9bf992c772a
Jesper A <eaajnat@students.eaaa.dk>
2023-10-24 20:56:11 +0200

    Add account DAO
    Introduces a DAO for creating accounts. This paves the way for an API
    POST endpoint for creating a new account.

--- src/main/kotlin/HovedopgApp.kt
+++ src/main/kotlin/HovedopgApp.kt
@@ -1,3 +1,4 @@
+import database.AccountDao
 import io.dropwizard.Application
 import io.dropwizard.jdbi3.JdbiFactory
 import io.dropwizard.setup.Environment
@@ -6,10 +7,9 @@ import org.jdbi.v3.core.Jdbi
 class HovedopgApp : Application<HovedopgConfiguration>() {
 
     override fun run(config: HovedopgConfiguration, env: Environment) {
-        // Create a DataSource using a connection pool (BasicDataSource in this example)
         val factory = JdbiFactory()
         val jdbi: Jdbi = factory.build(env, config.database, "mysql")
-//        val daoMeasurement = jdbi.onDemand(MeasurementsDao::class.java)
+        val accountDao = jdbi.onDemand(AccountDao::class.java)
     }
 
 }
---
+++ src/main/kotlin/database/AccountDao.kt
@@ -0,0 +1,15 @@
+package database
+
+import org.jdbi.v3.sqlobject.statement.GetGeneratedKeys
+import org.jdbi.v3.sqlobject.statement.SqlUpdate
+
+interface AccountDao {
+
+    @SqlUpdate("""
+        INSERT INTO accounts
+        VALUES ()
+    """)
+    @GetGeneratedKeys
+    fun create(): Int
+
+}
